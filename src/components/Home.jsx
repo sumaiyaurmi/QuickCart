c34a88640ffff8c09ce6541cfc4dd36c84db86ae
Sumaiya Urmi <akthersumaiya689@gmail.com>
2024-08-16 11:42:47 +0600

    footer created

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navber from './Navber';
+import Footer from './Footer';
 
 const Home = () => {
     return (
         <div>
             <Navber></Navber>
+
+            <Footer></Footer>
         </div>
     );
 };
